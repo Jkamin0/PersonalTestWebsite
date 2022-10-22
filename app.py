ca68318911b5c2d63b171f9b8832e71f6dd8cc59
Ubuntu <ubuntu@ip-172-31-27-84.us-west-2.compute.internal>
2022-10-22 01:50:22 +0000

    added on new page and links between them

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 @app.route("/")
+@app.route('/index')
 def hello_world():
-    return "<p>This is a super fancy website</p>"
+    return render_template('index.html')
+
+@app.route('/aboutme')
+def about_me():
+	return render_template('aboutMe.html')
+
+if __name__=="__main__":
+	app.run(host='0.0.0.0', port=5000)
 
